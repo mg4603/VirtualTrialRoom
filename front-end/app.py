be77c8525260658e15c2415f0d32d4440fa5ebff
mide <mide@localhost.localdomain>
2020-11-23 14:39:49 +0000

    edits to app.py

--- front-end/app.py
+++ front-end/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for, request, session, redirect, flash
 from flask_pymongo import PyMongo
-
+import numpy
 import bcrypt
+import cv2
 
 
 #intitalize flask app
@@ -10,9 +11,10 @@ app = Flask(__name__, static_url_path='/static')
 
 
 app.config['MONGO_DBNAME'] = 'Users'
-app.config['MONGO_URI'] = 'mongodb+srv://admin:<password>@cluster0.1f9yj.mongodb.net/Users?retryWrites=true&w=majority'
+app.config['MONGO_URI'] = 'mongodb+srv://admin:admin@cluster0.1f9yj.mongodb.net/Users?retryWrites=true&w=majority'
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
-
+app.config['img_dir_img'] = '../backend/data/test/image'
+app.config['img_dir_cloth'] = '../backend/data/test/cloth'
 mongo = PyMongo(app)
 
     
@@ -35,8 +37,32 @@ def aboutUs():
 def product():
     if request.method == 'POST':
         if request.files:
-            person = request.files['img_person']
-            article = request.files['img_article']
+            # person = request.files['img_person']
+            # article = request.files['img_article']
+            person = request.files['img_person'].read()
+            article = request.files['img_article'].read()
+            #convert string data to numpy array
+            npimg_article = numpy.fromstring(article, numpy.uint8)
+            npimg_person = numpy.fromstring(person, numpy.uint8)
+            # convert numpy array to image
+            img_person = cv2.imdecode(npimg_person, cv2.IMREAD_UNCHANGED)
+            img_article = cv2.imdecode(npimg_article, cv2.IMREAD_UNCHANGED)
+            cv2.imwrite(app.config['img_dir_img']+'/0_0.jpg', img_person)
+            cv2.imwrite(app.config['img_dir_cloth']+'/0_1.jpg', img_article)
+            file_obj = open(r'./backend/data/test_pairs.txt','w')
+            string = '0_0.jpg 0_1.jpg'
+            file_obj.write(string)
+            file_obj.close()
+			# person = request.files['img_person']
+			# article = request.files['img_article']
+			# person_filename = '1_p.jpg'
+			# article_filename = 	'1_a.jpg'
+			# # filename = secure_filename(file.filename) # save file 
+			# person_filepath = os.path.join(app.config['imgdir'], person_filename)
+			# article_filepath = os.path.join(app.config['imdir'], article_filename)
+
+			# file.save(filepath)
+			# cv2.imread(filepath)
             return redirect(url_for('solution'))
     
     return render_template('product.html')
